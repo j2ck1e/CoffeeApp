7e710f1bf6f39167f81e1c2a59d37b8e72957bdf
j2ck1e <j2ck1e86@gmail.com>
2023-12-23 12:04:53 +0300

    Added permissions and dependencies

---
+++ app/src/main/java/com/jcdesign/coffeeapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jcdesign.coffeeapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jcdesign.coffeeapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
