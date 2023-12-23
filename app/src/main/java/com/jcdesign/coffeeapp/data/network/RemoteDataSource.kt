7a7c08222bb5a3a712cccf36f99b691b1c60efb2
j2ck1e <j2ck1e86@gmail.com>
2023-12-23 22:14:08 +0300

    Added network package(AuthApi, RemoteDataSource, Resource)

---
+++ app/src/main/java/com/jcdesign/coffeeapp/data/network/RemoteDataSource.kt
@@ -0,0 +1,21 @@
+package com.jcdesign.coffeeapp.data.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RemoteDataSource {
+    companion object{
+        private const val BASE_URL = "http://147.78.66.203:3210/"
+    }
+
+    fun<Api> buildApi(
+        api: Class<Api>
+    ): Api {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(api)
+    }
+
+}
