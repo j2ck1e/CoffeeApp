7339abf8c688adf869537c1fb12d204471a3a043
j2ck1e <j2ck1e86@gmail.com>
2023-12-23 12:41:37 +0300

    Added auth fragments & nav_auth.xml

--- app/src/main/java/com/jcdesign/coffeeapp/presentation/MainActivity.kt
+++ app/src/main/java/com/jcdesign/coffeeapp/presentation/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.jcdesign.coffeeapp.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jcdesign.coffeeapp.R
+import com.jcdesign.coffeeapp.presentation.auth.AuthActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        finish()
+        startActivity(Intent(this, AuthActivity::class.java))
     }
 }
